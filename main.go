253e7d852a15bb5a462ec226bb8de004c5d2254f
strogiyotec <almas337519@gmail.com>
2021-06-27 07:34:59 -0700

    remove useless Prints

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"anytimes/http"
-	"fmt"
-	"os"
 )
 
 func main() {
-	token := os.Getenv("ANYTIME_SESSION_TOKEN")
-	sig := os.Getenv("ANYTIME_SESSION_SIG")
-	fmt.Printf("Token %s Sig %s \n", token, sig)
 	http.ReserverTime()
 }
